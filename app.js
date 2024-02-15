3cb5163b22236de3dea27813e8cf8955c3a6d7b3
AmrasHameed <amrazhameed@gmail.com>
2024-02-15 20:15:08 +0530

    initial commit

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const userRouter=require('./server/routers/user')
 const mongoose=require('mongoose')
 const nocache=require('nocache')
 const session= require('express-session')
-console.log("skdjfkdjsfhdjf");
-console.log("skdjfkdjsfhdjf");
 app.use(nocache())
 app.use(session({
     secret:'qwertyasdfghj',
